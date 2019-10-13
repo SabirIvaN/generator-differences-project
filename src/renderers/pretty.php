293712ccf62fd55979120b102e0f21ab82a7a941
Иван Сабиров <sabirivan@yandex.com>
2019-10-13 17:47:48 +0300

    Произведен рефакторинг

--- src/renderers/pretty.php
+++ src/renderers/pretty.php
@@ -11,17 +11,15 @@ function renderPretty($ast, $level = 0)
 {
     $indent = str_repeat('    ', $level);
     $changes = array_reduce($ast, function ($acc, $node) use ($indent, $level) {
+        $value = getValue($node['value'], $level);
         switch ($node['type']) {
             case 'not changed':
-                $value = getValue($node['value'], $level);
                 $acc[] = "{$indent}    {$node['key']}: {$value}";
                 break;
             case 'added':
-                $value = getValue($node['value'], $level);
                 $acc[] = "{$indent}  + {$node['key']}: {$value}";
                 break;
             case 'deleted':
-                $value = getValue($node['value'], $level);
                 $acc[] = "{$indent}  - {$node['key']}: {$value}";
                 break;
             case 'changed':
@@ -37,8 +35,7 @@ function renderPretty($ast, $level = 0)
         }
         return $acc;
     });
-    $result = implode(PHP_EOL, $changes);
-    return $result;
+    return implode(PHP_EOL, $changes);;
 }
 
 function getValue($data, $level)
