56618bf48b8bdf148aef66980c104183757408f8
Иван Сабиров <sabirivan@yandex.com>
2019-10-13 18:51:40 +0300

    Произведен рефакторинг

--- src/builder.php
+++ src/builder.php
@@ -16,40 +16,40 @@ function build($data1, $data2)
 function getData($key, $data1, $data2)
 {
     if (!array_key_exists($key, $data1)) {
-        $result = getData1($key, $data1, $data2);
+        $result = getAdded($key, $data1, $data2);
     } elseif (!array_key_exists($key, $data2)) {
-        $result = getData2($key, $data1, $data2);
+        $result = getDeleted($key, $data1, $data2);
     } elseif (is_array($data1[$key]) && is_array($data2[$key])) {
-        $result = getData3($key, $data1, $data2);
+        $result = getParent($key, $data1, $data2);
     } elseif ($data1[$key] === $data2[$key]) {
-        $result = getData4($key, $data1, $data2);
+        $result = getNotChanged($key, $data1, $data2);
     } elseif ($data1[$key] !== $data2[$key]) {
-        $result = getData5($key, $data1, $data2);
+        $result = getChanged($key, $data1, $data2);
     }
     return $result;
 }
 
-function getData1($key, $data1, $data2)
+function getAdded($key, $data1, $data2)
 {
     return ['type' => 'added', 'key' => $key, 'value' => $data2[$key]];
 }
 
-function getData2($key, $data1, $data2)
+function getDeleted($key, $data1, $data2)
 {
     return ['type' => 'deleted', 'key' => $key, 'value' => $data1[$key]];
 }
 
-function getData3($key, $data1, $data2)
+function getParent($key, $data1, $data2)
 {
-    return ['type' => 'parent', 'key' => $key, 'children' => generateAst($data1[$key], $data2[$key])];
+    return ['type' => 'parent', 'key' => $key, 'children' => build($data1[$key], $data2[$key])];
 }
 
-function getData4($key, $data1, $data2)
+function getNotChanged($key, $data1, $data2)
 {
     return ['type' => 'not changed', 'key' => $key, 'value' => $data1[$key]];
 }
 
-function getData5($key, $data1, $data2)
+function getChanged($key, $data1, $data2)
 {
     return ['type' => 'changed', 'key' => $key, 'oldValue' => $data1[$key], 'newValue' => $data2[$key]];
 }
