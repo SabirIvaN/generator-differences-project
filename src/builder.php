32d128148a357fd925786a910249a8953ce4ed8c
Иван Сабиров <sabirivan@yandex.com>
2019-10-13 18:32:20 +0300

    Произведен рефакторинг, добавлены функции рефакторинга

--- src/builder.php
+++ src/builder.php
@@ -16,31 +16,40 @@ function build($data1, $data2)
 function getData($key, $data1, $data2)
 {
     if (!array_key_exists($key, $data1)) {
-        $result = ['type' => 'added',
-                   'key' => $key,
-                   'value' => $data2[$key]
-                  ];
+        $result = getData1($key, $data1, $data2);
     } elseif (!array_key_exists($key, $data2)) {
-        $result = ['type' => 'deleted',
-                   'key' => $key,
-                   'value' => $data1[$key]
-                  ];
+        $result = getData2($key, $data1, $data2);
     } elseif (is_array($data1[$key]) && is_array($data2[$key])) {
-        $result = ['type' => 'parent',
-                   'key' => $key,
-                   'children' => generateAst($data1[$key], $data2[$key])
-                  ];
+        $result = getData3($key, $data1, $data2);
     } elseif ($data1[$key] === $data2[$key]) {
-        $result = ['type' => 'not changed',
-                   'key' => $key,
-                   'value' => $data1[$key]
-                  ];
+        $result = getData4($key, $data1, $data2);
     } elseif ($data1[$key] !== $data2[$key]) {
-        $result = [
-                   'type' => 'changed',
-                   'key' => $key,
-                   'oldValue' => $data1[$key],
-                   'newValue' => $data2[$key]
-                  ];
+        $result = getData5($key, $data1, $data2);
     }
+    return $result;
+}
+
+function getData1($key, $data1, $data2)
+{
+    return ['type' => 'added', 'key' => $key, 'value' => $data2[$key]];
+}
+
+function getData2($key, $data1, $data2)
+{
+    return ['type' => 'deleted', 'key' => $key, 'value' => $data1[$key]];
+}
+
+function getData3($key, $data1, $data2)
+{
+    return ['type' => 'parent', 'key' => $key, 'children' => generateAst($data1[$key], $data2[$key])];
+}
+
+function getData4($key, $data1, $data2)
+{
+    return ['type' => 'not changed', 'key' => $key, 'value' => $data1[$key]];
+}
+
+function getData5($key, $data1, $data2)
+{
+    return ['type' => 'changed', 'key' => $key, 'oldValue' => $data1[$key], 'newValue' => $data2[$key]];
 }
