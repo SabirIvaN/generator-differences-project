4457bd3f9ddef09e933b50675532fddc5757a3ec
Иван Сабиров <sabirivan@yandex.com>
2019-10-25 10:31:54 +0300

    Сделан пятый шаг

--- src/parser.php
+++ src/parser.php
@@ -2,11 +2,17 @@
 
 namespace GenDiff\Parser;
 
+use Symfony\Component\Yaml\Yaml;
+
 function parse($data, $dataType)
 {
     switch ($dataType) {
         case "json":
             $result = json_decode($data, true);
+            break;
+        case "yaml":
+        case "yml":
+            $result = Yaml::parse($data);
             break;
         default:
             throw new \Exception("Unknown data type");
