78631813716ef9e7adb113d3a38a7985f112ad8c
Иван Сабиров <sabirivan@yandex.com>
2019-11-03 22:38:03 +0300

    Написаны тесты для plain формата

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -8,7 +8,7 @@ use function GenDiff\Runner\run;
 
 class GenDiffTests extends TestCase
 {
-    public function testPrettyResult()
+    public function testPrettyFlatResult()
     {
         $path1 = "tests/fixtures/flatBefore.json";
         $path2 = "tests/fixtures/flatAfter.json";
@@ -18,7 +18,7 @@ class GenDiffTests extends TestCase
         $this->assertEquals($expected, run($path1, $path2, "pretty"));
         $this->assertEquals($expected, run($path3, $path4, "pretty"));
     }
-    public function testNestedResult()
+    public function testPrettyNestedResult()
     {
       $path1 = "tests/fixtures/nestedBefore.json";
       $path2 = "tests/fixtures/nestedAfter.json";
@@ -26,5 +26,24 @@ class GenDiffTests extends TestCase
       $path4 = "tests/fixtures/nestedAfter.yaml";
       $expected = file_get_contents("tests/fixtures/expected/prettyNestedResult.txt");
       $this->assertEquals($expected, run($path1, $path2, "pretty"));
+    }
+    public function testPlainFlatResult()
+    {
+        $path1 = "tests/fixtures/flatBefore.json";
+        $path2 = "tests/fixtures/flatAfter.json";
+        $path3 = "tests/fixtures/flatBefore.yaml";
+        $path4 = "tests/fixtures/flatAfter.yaml";
+        $expected = file_get_contents("tests/fixtures/expected/plainFlatResult.txt");
+        $this->assertEquals($expected, run($path1, $path2, "plain"));
+        $this->assertEquals($expected, run($path3, $path4, "plain"));
+    }
+    public function testPlainNestedResult()
+    {
+      $path1 = "tests/fixtures/nestedBefore.json";
+      $path2 = "tests/fixtures/nestedAfter.json";
+      $path3 = "tests/fixtures/nestedBefore.yaml";
+      $path4 = "tests/fixtures/nestedAfter.yaml";
+      $expected = file_get_contents("tests/fixtures/expected/plainNestedResult.txt");
+      $this->assertEquals($expected, run($path1, $path2, "plain"));
     }
 }
