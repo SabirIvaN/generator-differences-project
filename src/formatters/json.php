6ef44ca37e87a1f834b4b59ae4c9ae414acaeaa8
Иван Сабиров <sabirivan@yandex.com>
2019-11-04 13:36:40 +0300

    Испраленны ошибки выявленные при тестировании

--- src/formatters/json.php
+++ src/formatters/json.php
@@ -2,14 +2,14 @@
 
 namespace GenDiff\Formatters\Json;
 
-function runJsonRender ($ast)
+function runJsonRender($ast)
 {
     return renderJson($ast);
 }
 
-function renderJson ($ast)
+function renderJson($ast)
 {
-    $result = json_encode($ast, JSON_PRETTY_PRINT);
+    $result = json_encode($ast, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
     $result = trim($result, "[]");
     $result = "{{$result}}";
     return $result;
