f949a69dbf398bedfed6768d978d0b2e718e2528
Иван Сабиров <sabirivan@yandex.com>
2019-11-10 23:00:02 +0300

    Подготовлен релиз версии 1.1

--- src/runner.php
+++ src/runner.php
@@ -21,7 +21,7 @@ function run()
     DOC;
 
     try {
-        $args = \Docopt::handle($doc, array("version" => "GenDiff 1.0"));
+        $args = \Docopt::handle($doc, array("version" => "GenDiff 1.1"));
         $diff = render($args["<firstFile>"], $args["<secondFile>"], $args["--format"]);
         print_r($diff);
     } catch (\Exception $e) {
